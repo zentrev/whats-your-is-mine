99e09f2b46798d2612fe84394670f383056aca29
Thumpax <c.t.raines.iii@gmail.com>
2019-05-06 09:50:43 -0600

    broken pessent

---
+++ Assets/Scripts/ScriptableObjects/AgentData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "AgentData")]
+public class AgentData : ScriptableObject
+{
+    [System.Flags]
+    public enum eAgentAttributes
+    {
+        NONE        = (1 << 0),
+        KEYHOLDER   = (1 << 1),
+        GUARD       = (1 << 2),
+    }
+
+    public float Value = 10.0f;
+    [Range(0.0f, 1.0f)]public float Awarness = .5f;
+    public eAgentAttributes Attributes = eAgentAttributes.NONE;
+}
