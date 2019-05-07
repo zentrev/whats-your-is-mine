ebf9f780b47c6edb4a5d05abd1263e48c1ffa69d
Thumpax <c.t.raines.iii@gmail.com>
2019-05-07 11:56:38 -0600

    agent creation on start

--- Assets/Scripts/Agent/AgentBehavior.cs
+++ Assets/Scripts/Agent/AgentBehavior.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class AgentBehavior : MonoBehaviour
 {
-    [SerializeField] public AgentData = null;
+    [SerializeField] AgentData m_agentData;
+
     void Start()
     {
         
     }
 
+    public void Init(AgentData agentData)
+    {
+        m_agentData = agentData;
+        // do starting things
+    }
+
     void Update()
     {
-        
+        // AI and behavior things
     }
 }
---
+++ Assets/Scripts/Agent/AgentManager.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentManager : Singleton<AgentManager>
+{
+    [System.Serializable]
+    public struct LevelAgents
+    {
+        public AgentData agentData;
+        public bool forceAgent;
+        [Range(0.0f, 1.0f)] public float agentProbobiltiy;
+        public bool singleton;
+        [HideInInspector] public bool inLevel;
+    }
+
+    [System.Serializable]
+    public struct AgentSpawner
+    {
+        public Transform transform;
+        public int count;
+    }
+
+    [System.Serializable]
+    public struct FixedAgents
+    {
+        public AgentSpawner agentSpawner;
+        public AgentData agentData;
+    }
+
+    [SerializeField] GameObject m_agentBase = null;
+    [SerializeField] List<FixedAgents> m_fixedAgents = new List<FixedAgents>();
+    [SerializeField] List<LevelAgents> m_posibleAgents = new List<LevelAgents>();
+    [SerializeField] List<AgentSpawner> m_agentSpawners = new List<AgentSpawner>();
+    List<AgentBehavior> m_levelAgents = new List<AgentBehavior>();
+
+    public void Start()
+    {
+        SpwanAgents();
+    }
+
+    public void SpwanAgents()
+    {
+        foreach(FixedAgents fixedAgent in m_fixedAgents)
+        {
+            GameObject agent = GameObject.Instantiate(m_agentBase);
+            agent.transform.position = fixedAgent.agentSpawner.transform.position;
+            if(!agent.GetComponent<AgentBehavior>()) agent.AddComponent<AgentBehavior>();
+            agent.GetComponent<AgentBehavior>().Init(fixedAgent.agentData);
+            m_levelAgents.Add(agent.GetComponent<AgentBehavior>());
+        }
+
+        float agentWheights = 0.0f;
+        foreach(LevelAgents posibleAgent in m_posibleAgents)
+        {
+            LevelAgents pAgent = posibleAgent;
+
+            if (pAgent.forceAgent)
+            {
+                int spwanerIndex = Random.Range(0, m_agentSpawners.Count);
+                AgentSpawner spawn = m_agentSpawners[spwanerIndex];
+
+                GameObject agent = GameObject.Instantiate(m_agentBase);
+                agent.transform.position = spawn.transform.position;
+
+                if (!agent.GetComponent<AgentBehavior>()) agent.AddComponent<AgentBehavior>();
+                agent.GetComponent<AgentBehavior>().Init(pAgent.agentData);
+                m_levelAgents.Add(agent.GetComponent<AgentBehavior>());
+                pAgent.inLevel = true;
+
+                spawn.count--;
+                m_agentSpawners[spwanerIndex] = spawn;
+            }
+            agentWheights += posibleAgent.agentProbobiltiy;
+        }
+
+        for(int i = 0; i < m_agentSpawners.Count; i++)
+        {
+            AgentSpawner spawn = m_agentSpawners[i];
+            Debug.Log("SPAWN COUNT: " + spawn.count);
+            while(spawn.count > 0)
+            {
+                float wheight = Random.Range(0.0f, agentWheights);
+                for(int j = 0; j < m_posibleAgents.Count; j++)
+                {
+                    LevelAgents pAgent = m_posibleAgents[j];
+
+                    Debug.Log(pAgent.agentData + " : " + pAgent.inLevel);
+                    if (!pAgent.singleton || (pAgent.singleton && !pAgent.inLevel))
+                    {
+                        wheight -= pAgent.agentProbobiltiy;
+                        if (wheight <= 0)
+                        {
+                            GameObject agent = GameObject.Instantiate(m_agentBase);
+                            agent.transform.position = spawn.transform.position;
+
+                            if (!agent.GetComponent<AgentBehavior>()) agent.AddComponent<AgentBehavior>();
+                            agent.GetComponent<AgentBehavior>().Init(pAgent.agentData);
+                            m_levelAgents.Add(agent.GetComponent<AgentBehavior>());
+                            pAgent.inLevel = true;
+                            m_posibleAgents[j] = pAgent;
+                            spawn.count--;
+                            break;
+                        }
+                    }
+                }
+            }
+            m_agentSpawners[i] = spawn;
+
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
