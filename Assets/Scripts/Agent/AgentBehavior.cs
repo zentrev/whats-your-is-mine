6326b2ea9bbb4aba1e61fcf0f194af20e173ed0a
Thumpax <c.t.raines.iii@gmail.com>
2019-05-07 09:18:21 -0600

    pull comit

---
+++ Assets/Scripts/Agent/AgentBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentBehavior : MonoBehaviour
+{
+    [SerializeField] public AgentData = null;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
