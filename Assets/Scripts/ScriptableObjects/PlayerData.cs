446b4c5ddf785e4be0f14d393a42fd8811219ded
Yoannyz <yzamora@student.neumont.edu>
2019-05-10 09:50:14 -0600

    PlayerData and Store
    Added a scriptable object for player and a store script

---
+++ Assets/Scripts/ScriptableObjects/PlayerData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "AgentData")]
+public class PlayerData : ScriptableObject
+{
+    List<float> itemsHeld = new List<float>();
+    public float money = 0.0f;
+    public float time = 0.0f;
+}
