2a232467530e177a1877c3dd6d1b57a85b621cc6
Yoannyz <yzamora@student.neumont.edu>
2019-05-09 22:59:22 -0600

    Basic Menu Nav

---
+++ Assets/Scripts/Common/MenuNavigation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuNavigation : MonoBehaviour
+{
+
+    public void OnSceneButtonClick(string nextScene)
+    {
+        GameManager.Instance.LoadLevel(nextScene);
+    }
+}
