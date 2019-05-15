c1b7faccc13462f54b58db3d5ddbca3d97d9943d
Yoannyz <yzamora@student.neumont.edu>
2019-05-14 21:16:37 -0600

    Worked on buying from store
    Should work but getting an issue with number format exception

--- Assets/Scripts/Store/BuyingFromStore.cs
+++ Assets/Scripts/Store/BuyingFromStore.cs
@@ -6,6 +6,8 @@ using TMPro;
 public class BuyingFromStore : MonoBehaviour
 {
     [SerializeField] PlayerData m_playerData = null;
+    [SerializeField] TextMeshProUGUI costText = null;
+    [SerializeField] TextMeshProUGUI upgradeText = null;
 
     public void Bought(string costString, string upgradeString)
     {
@@ -14,12 +16,13 @@ public class BuyingFromStore : MonoBehaviour
         if(m_playerData.money > cost)
         {
             m_playerData.time = m_playerData.time + upgradeAmount;
+            m_playerData.money = m_playerData.money - cost;
         }
     }
 
-    public void OnStoreButtonClick(TextMeshProUGUI cost, TextMeshProUGUI upgradeAmount)
+    public void OnStoreButtonClick()
     {
-        Bought(cost.ToString(), upgradeAmount.ToString());
+        Bought(costText.ToString(), upgradeText.ToString());
     }
 
 
